2b8fc1f951919b4c6051c88e4682c88f9d666cb1
farahmango <farahmangoo@gmail.com>
2023-04-19 22:52:11 +0300

    add items

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
@@ -6,38 +8,45 @@ import org.testng.annotations.Test;
 
 public class MyTestCases extends Parameters{
 //	PasswordGenerator myObj = new PasswordGenerator();
-	String password2 = Parameters.PasswordGenerator.generatePassword();
+//	String password2 = Parameters.PasswordGenerator.generatePassword();
+	
+	
+	
+	String[] URLS = { "https://magento.softwaretestingboard.com/radiant-tee.html",
+			"https://magento.softwaretestingboard.com/breathe-easy-tank.html",
+			"https://magento.softwaretestingboard.com/argus-all-weather-tank.html",
+			"https://magento.softwaretestingboard.com/hero-hoodie.html",
+			"https://magento.softwaretestingboard.com/push-it-messenger-bag.html" };
 	
 	@BeforeTest
 	public void beforeTest() {
 		driver.get(homeUrl);
 		
-		System.out.println(password2 + " before");
 
 	}
-	
-	@Test(priority =1)
-	public void signUp() {
-		driver.get(signUpUrl);
-		
-		WebElement firstName = driver.findElement(By.xpath("//input[@id='firstname']"));
-		WebElement lastName =driver.findElement(By.xpath("//input[@id='lastname']"));
-		WebElement email =driver.findElement(By.xpath("//input[@id='email_address']"));
-		WebElement password =driver.findElement(By.xpath("//input[@id='password']"));
-		WebElement confirmPassword =driver.findElement(By.xpath("//input[@id='password-confirmation']"));
-		WebElement clickButton = driver.findElement(By.xpath("//body/div[1]/main[1]/div[3]/div[1]/form[1]/div[1]/div[1]/button[1]"));
-
-		System.out.println(password2 + " signUp");
-		
-		firstName.sendKeys(this.firstName);
-		lastName.sendKeys(this.lastName);
-		email.sendKeys(this.email);
-		password.sendKeys(password2);
-		confirmPassword.sendKeys(password2);
-		clickButton.click();
-		
-	}
-	
+//	
+//	@Test(priority =1)
+//	public void signUp() {
+//		driver.get(signUpUrl);
+//		
+//		WebElement firstName = driver.findElement(By.xpath("//input[@id='firstname']"));
+//		WebElement lastName =driver.findElement(By.xpath("//input[@id='lastname']"));
+//		WebElement email =driver.findElement(By.xpath("//input[@id='email_address']"));
+//		WebElement password =driver.findElement(By.xpath("//input[@id='password']"));
+//		WebElement confirmPassword =driver.findElement(By.xpath("//input[@id='password-confirmation']"));
+//		WebElement clickButton = driver.findElement(By.xpath("//body/div[1]/main[1]/div[3]/div[1]/form[1]/div[1]/div[1]/button[1]"));
+//
+//		System.out.println(pass + " signUp");
+//		
+//		firstName.sendKeys(this.firstName);
+//		lastName.sendKeys(this.lastName);
+//		email.sendKeys(this.email);
+//		password.sendKeys(pass);
+//		confirmPassword.sendKeys(pass);
+//		clickButton.click();
+//		
+//	}
+//	
 	@Test(priority = 2)
 	public void signIn() {
 		driver.get(signOutUrl);
@@ -46,19 +55,154 @@ public class MyTestCases extends Parameters{
 		WebElement email =driver.findElement(By.xpath("//input[@id='email']"));
 		WebElement password =driver.findElement(By.xpath("//body/div[1]/main[1]/div[3]/div[1]/div[2]/div[1]/div[2]/form[1]/fieldset[1]/div[3]/div[1]/input[1]"));
 		WebElement clickButton = driver.findElement(By.xpath("//body/div[1]/main[1]/div[3]/div[1]/div[2]/div[1]/div[2]/form[1]/fieldset[1]/div[4]/div[1]/button[1]"));
-		System.out.println(password2 + " signIn");
 
 		email.sendKeys(this.email);
-		password.sendKeys(password2);
+		password.sendKeys(this.pass);
+
 		clickButton.click();
 
 		
 	}
 	
+	@Test(priority = 3)
+	public void addItems() throws InterruptedException {
+
+	
+		for (int i = 0; i < URLS.length; i++) {
+			driver.get(URLS[i]);
+
+			if (driver.getCurrentUrl().contains("tee")) {
+				Thread.sleep(3000);
+
+				WebElement divItemSize = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+				List<WebElement> size =  divItemSize.findElements(By.className("text"));
+				System.out.println(size.size());
+				
+				WebElement divItemColor = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[2]/div[1]"));
+				List<WebElement> color =  divItemColor.findElements(By.className("color"));
+				System.out.println(color.size());
+
+				int randSize = myRandom.nextInt(size.size());
+				int randColor = myRandom.nextInt(color.size());
+				
+				size.get(randSize).click();
+				color.get(randColor).click();
+
+				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
+				Quintyt.clear();
+				Quintyt.sendKeys("3");
+				
+				WebElement addCart = driver.findElement(By.xpath("//button[@id='product-addtocart-button']"));
+				addCart.click();
+
+				
+			} else if (driver.getCurrentUrl().contains("easy")) {
+				Thread.sleep(3000);
+				
+				WebElement divItemSize = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+				List<WebElement> size =  divItemSize.findElements(By.className("text"));
+				System.out.println(size.size());
+				Thread.sleep(3000);
+				
+				WebElement divItemColor = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[2]/div[1]"));
+				List<WebElement> color =  divItemColor.findElements(By.className("color"));
+				System.out.println(color.size());
+				
+				int randSize = myRandom.nextInt(size.size());
+				int randColor = myRandom.nextInt(color.size());
+				
+				size.get(randSize).click();
+				color.get(randColor).click();
+
+				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
+				Quintyt.clear();
+				Quintyt.sendKeys("2");
+				
+				WebElement addCart = driver.findElement(By.xpath("//button[@id='product-addtocart-button']"));
+				addCart.click();
+
+				
+			} else if (driver.getCurrentUrl().contains("weather")) {
+				
+				Thread.sleep(3000);
+				WebElement divItemSize = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+				List<WebElement> size =  divItemSize.findElements(By.className("text"));
+				System.out.println(size.size());
+				Thread.sleep(3000);
+				
+				WebElement divItemColor = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[2]/div[1]"));
+				List<WebElement> color =  divItemColor.findElements(By.className("color"));
+				System.out.println(color.size());
+				
+				int randSize = myRandom.nextInt(size.size());
+				int randColor = myRandom.nextInt(color.size());
+				
+				size.get(randSize).click();
+				color.get(randColor).click();
+
+				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
+				Quintyt.clear();
+				Quintyt.sendKeys("4");
+				
+				WebElement addCart = driver.findElement(By.xpath("//button[@id='product-addtocart-button']"));
+				addCart.click();
+
+			} 
+			else if (driver.getCurrentUrl().contains("hoodie")) {
+			
+				Thread.sleep(3000);
+				WebElement divItemSize = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+				List<WebElement> size =  divItemSize.findElements(By.className("text"));
+				System.out.println(size.size());
+				Thread.sleep(3000);
+				
+				WebElement divItemColor = driver.findElement(By.xpath("//body/div[1]/main[1]/div[2]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[2]/div[1]"));
+				List<WebElement> color =  divItemColor.findElements(By.className("color"));
+				System.out.println(color.size());
+				
+				int randSize = myRandom.nextInt(size.size());
+				int randColor = myRandom.nextInt(color.size());
+				
+				size.get(randSize).click();
+				color.get(randColor).click();
+
+				WebElement Quintyt = driver.findElement(By.xpath("//*[@id=\"qty\"]"));
+				Quintyt.clear();
+				Quintyt.sendKeys("1");
+				
+				WebElement addCart = driver.findElement(By.xpath("//button[@id='product-addtocart-button']"));
+				addCart.click();
+
+			} else if (driver.getCurrentUrl().contains("messenger")) {
+					
+				WebElement Quintyt = driver.findElement(By.xpath("//input[@id='qty']"));
+				Quintyt.clear();
+				Quintyt.sendKeys("5");
+				
+				WebElement addCart = driver.findElement(By.xpath("//button[@id='product-addtocart-button']"));
+				addCart.click();
+
+			}
+		}
+		Thread.sleep(5000);
+
+		WebElement basketContent = driver.findElement(By.className("counter-number"));
+		String basket = basketContent.getText();
+		System.out.println(basket);
+		
+		String expectedBasket="15";
+		Thread.sleep(3000);
+		myAssert.assertEquals(basket, expectedBasket);
+		myAssert.assertAll();
+		
+	}
+	
+	
+	
+	
 	@AfterTest()
 	public void afterTest() {
-		System.out.println(password2 + " after");
-		driver.quit();
+//		driver.quit();
 		
 	}
 	
--- src/Parameters.java
+++ src/Parameters.java
@@ -1,13 +1,15 @@
 import java.security.SecureRandom;
+import java.util.Random;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.asserts.SoftAssert;
 
 public class Parameters {
 
 	
 	WebDriver driver =new EdgeDriver();
-//	SoftAssert myAssert = new SoftAssert();
+	SoftAssert myAssert = new SoftAssert();
 	String homeUrl = "https://magento.softwaretestingboard.com/";
 	String signUpUrl = "https://magento.softwaretestingboard.com/customer/account/create/";
 	String signOutUrl = "https://magento.softwaretestingboard.com/customer/account/logout/";
@@ -15,30 +17,32 @@ public class Parameters {
 	
 	String firstName = "farah";
 	String lastName = "go";
-	String email = "farah@gmail.com";
-
+	String email = "farah502@gmail.com";
+	String pass = "farah@123";
+	Random myRandom = new Random();
 	
 	
-	public class PasswordGenerator {
-	    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
-	    private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
-	    private static final String DIGIT = "0123456789";
-	    private static final String SPECIAL_CHAR = "!@#$%^&*()_-+=<>?/{}|~";
-
-	    private static final String PASSWORD_CHARS = CHAR_LOWER + CHAR_UPPER + DIGIT + SPECIAL_CHAR;
-	    private static final int PASSWORD_LENGTH = 12;
-	    int a = 10 ; 
-	    public static String generatePassword() {
-	        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
-	        SecureRandom random = new SecureRandom();
-
-	        for (int i = 0; i < PASSWORD_LENGTH; i++) {
-	            int randomCharIndex = random.nextInt(PASSWORD_CHARS.length());
-	            password.append(PASSWORD_CHARS.charAt(randomCharIndex));
-	        }
-
-	        return password.toString();
-	    }
-	}
+//	
+//	public class PasswordGenerator {
+//	    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+//	    private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+//	    private static final String DIGIT = "0123456789";
+//	    private static final String SPECIAL_CHAR = "!@#$%^&*()_-+=<>?/{}|~";
+//
+//	    private static final String PASSWORD_CHARS = CHAR_LOWER + CHAR_UPPER + DIGIT + SPECIAL_CHAR;
+//	    private static final int PASSWORD_LENGTH = 12;
+//	    int a = 10 ; 
+//	    public static String generatePassword() {
+//	        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+//	        SecureRandom random = new SecureRandom();
+//
+//	        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+//	            int randomCharIndex = random.nextInt(PASSWORD_CHARS.length());
+//	            password.append(PASSWORD_CHARS.charAt(randomCharIndex));
+//	        }
+//
+//	        return password.toString();
+//	    }
+//	}
 }
 
